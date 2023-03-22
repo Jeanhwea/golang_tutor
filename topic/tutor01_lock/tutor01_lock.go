2a0aa61dd71a8390d70a2a02c15cd3e8293eefa4
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-03-22 23:45:26 +0800

    timestamp: 2023-03-22 23:45:26

---
+++ topic/tutor01_lock/tutor01_lock.go
@@ -0,0 +1,21 @@
+package tutor01_lock
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(1)
+
+	fmt.Println("The program starts ...")
+
+	go func() {
+		for {
+		}
+	}()
+
+	time.Sleep(time.Second)
+	fmt.Println("I got scheduled!")
+}
---
+++ topic/tutor01_lock/tutor01_lock_test.go
@@ -0,0 +1,9 @@
+package tutor01_lock
+
+import (
+	"testing"
+)
+
+func TestTutor01Lock01(t *testing.T) {
+	main()
+}
