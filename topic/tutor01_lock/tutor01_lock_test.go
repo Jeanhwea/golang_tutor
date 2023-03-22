ac0e8455644797537d89ac2eac2905dde56f7f83
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-03-22 23:56:24 +0800

    add

--- topic/tutor01_lock/tutor01_lock_test.go
+++ topic/tutor01_lock/tutor01_lock_test.go
@@ -1,9 +1,57 @@
 package tutor01_lock
 
 import (
+	"sync"
+	"sync/atomic"
 	"testing"
 )
 
 func TestTutor01Lock01(t *testing.T) {
 	main()
+}
+
+func Benchmark_RWMutex(b *testing.B) {
+	var lock sync.RWMutex
+	m := map[int]int{1: 2}
+
+	for i := 0; i < b.N; i++ {
+		lock.RLock()
+		_ = m[1]
+		lock.RUnlock()
+	}
+}
+
+func Benchmark_Atomic(b *testing.B) {
+	var ptr atomic.Value
+	ptr.Store(map[int]int{1: 2})
+
+	for i := 0; i < b.N; i++ {
+		m := ptr.Load().(map[int]int)
+		_ = m[1]
+	}
+}
+
+func Benchmark_RWMutex_parallel(b *testing.B) {
+	var lock sync.RWMutex
+	m := map[int]int{1: 2}
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			lock.RLock()
+			_ = m[1]
+			lock.RUnlock()
+		}
+	})
+}
+
+func Benchmark_Atomic_parallel(b *testing.B) {
+	var ptr atomic.Value
+	ptr.Store(map[int]int{1: 2})
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			m := ptr.Load().(map[int]int)
+			_ = m[1]
+		}
+	})
 }
