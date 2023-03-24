671cb351241a39ce22ef6084e7af2f05055fa4a1
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-03-24 11:23:37 +0800

    add

--- topic/tutor01_cas/tutor01_cas.go
+++ topic/tutor01_cas/tutor01_cas.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"sync/atomic"
 )
 
 var (
-	readCounter int64 = 1
+	readCounter int64
 )
 
 func main() {
 	atomic.AddInt64(&readCounter, 1)
 	atomic.AddInt64(&readCounter, 1)
+	fmt.Println(readCounter)
 }
