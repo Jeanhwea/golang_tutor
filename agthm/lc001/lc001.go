2b88218efe5ca13ec9d00b5195fb6bfd78e2e9d1
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-04-21 21:20:33 +0800

    update

---
+++ agthm/lc001/lc001.go
@@ -0,0 +1,15 @@
+// Copyright 2023 The Go Authors in ByteDance Inc. All rights reserved.
+// Use of this source code should follow terms of ByteDance Inc.
+//
+// Authors:     hujinghui.jeffrey@bytedance.com
+// Create Date: 2023-04-21
+// Update Date: 2023-04-21
+//
+
+package lc001
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+}
