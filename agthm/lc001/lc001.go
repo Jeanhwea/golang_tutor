764e755dd96d3025cfeaebdd5b8708080c1a7e8c
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-04-22 14:48:26 +0800

    update

--- agthm/lc001/lc001.go
+++ agthm/lc001/lc001.go
@@ -1,15 +1,14 @@
-// Copyright 2023 The Go Authors in ByteDance Inc. All rights reserved.
-// Use of this source code should follow terms of ByteDance Inc.
-//
-// Authors:     hujinghui.jeffrey@bytedance.com
-// Create Date: 2023-04-21
-// Update Date: 2023-04-21
-//
+// https://leetcode.cn/problems/two-sum/
 
 package lc001
 
-import "fmt"
+import (
+	"fmt"
+)
 
+func twoSum(nums []int, target int) (ans []int) {
+	return
+}
 func main() {
 	fmt.Println("hello")
 }
