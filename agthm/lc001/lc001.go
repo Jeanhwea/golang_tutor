8640a9cc690d5e3c3e8f329e9d368b402544e29d
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-04-29 21:14:10 +0800

    feat: lc001

--- agthm/lc001/lc001.go
+++ agthm/lc001/lc001.go
@@ -2,13 +2,13 @@
 
 package lc001
 
-import (
-	"fmt"
-)
-
 func twoSum(nums []int, target int) (ans []int) {
+	cache := make(map[int]int)
+	for i, a := range nums {
+		if j, ok := cache[a]; ok {
+			return []int{j, i}
+		}
+		cache[target-a] = i
+	}
 	return
-}
-func main() {
-	fmt.Println("hello")
 }
