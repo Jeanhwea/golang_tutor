4d399212920545cf5dfe97db156e02e3eafe91a6
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-04-30 17:36:21 +0800

    feat: 添加代码

--- agthm/lc003/lc003.go
+++ agthm/lc003/lc003.go
@@ -1,5 +1,35 @@
 package lc003
 
 func lengthOfLongestSubstring(s string) (ans int) {
+	n := len(s)
+	m := make(map[byte]bool, 128) // 保存 s[i:j-1] 的字符
+
+	for i, j := 0, 0; i < n; i++ {
+		for ; j < n; j++ {
+			// fmt.Printf("i=%v, j=%v, substr=%v\n", i, j, s[i:j])
+			if ok := m[s[j]]; ok {
+				ans = max(ans, j-i)
+				break
+			} else {
+				m[s[j]] = true
+			}
+		}
+
+		if j == n {
+			ans = max(ans, j-i)
+			return
+		}
+
+		// i 将会增加, 这个先置空
+		m[s[i]] = false
+	}
+
 	return
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- agthm/lc003/lc003_test.go
+++ agthm/lc003/lc003_test.go
@@ -5,4 +5,19 @@ import (
 )
 
 func Test_LC003_01(t *testing.T) {
+	str := "abcabcbb"
+	ans := lengthOfLongestSubstring(str)
+	t.Log(ans)
+}
+
+func Test_LC003_02(t *testing.T) {
+	str := "pwwkew"
+	ans := lengthOfLongestSubstring(str)
+	t.Log(ans)
+}
+
+func Test_LC003_03(t *testing.T) {
+	str := " "
+	ans := lengthOfLongestSubstring(str)
+	t.Log(ans)
 }
