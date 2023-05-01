3de1076e6e0d21c54f75f52d022a104a072c8a9e
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-01 18:21:01 +0800

    timestamp: 2023-05-01 18:21:01

--- agthm/lc020/lc020_test.go
+++ agthm/lc020/lc020_test.go
@@ -18,3 +18,11 @@ func Test_LC020_03(t *testing.T) {
 	s := "]"
 	t.Log(isValid(s))
 }
+
+func Test_LC020_04(t *testing.T) {
+	stack := []byte{}
+	stack = append(stack, 'a')
+	t.Log(stack)
+	stack = append([]byte{'b'}, stack...)
+	t.Log(stack)
+}
