a8d41b85f12e7d10b791bd9207447c6401ccbbea
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-02 23:45:25 +0800

    timestamp: 2023-05-02 23:45:24

--- agthm/lc088/lc088_test.go
+++ agthm/lc088/lc088_test.go
@@ -11,3 +11,12 @@ func Test_LC088_01(t *testing.T) {
 	merge(nums1, m, nums2, n)
 	t.Log(nums1)
 }
+
+func Benchmark_LC088_01(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		nums1 := []int{1, 2, 3, 0, 0, 0}
+		m, n := 3, 3
+		nums2 := []int{2, 5, 6}
+		merge(nums1, m, nums2, n)
+	}
+}
