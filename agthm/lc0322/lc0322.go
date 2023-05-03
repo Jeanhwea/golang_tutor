9d16d95bcdc37674b65c2a8430b5620f32c6c29a
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-03 20:44:03 +0800

    timestamp: 2023-05-03 20:44:03

--- agthm/lc0322/lc0322.go
+++ agthm/lc0322/lc0322.go
@@ -1,1 +1,56 @@
 package lc0322
+
+import (
+	"math"
+)
+
+// 零钱兑换
+//
+//    广度优先搜索
+//    dp[n] := 1 + min(dp[n-c]), c belongs to coins
+//          |= 0  iff n == 0
+//          |= -1 iff n < 0
+//
+func coinChange(coins []int, amount int) (ans int) {
+	memo = make(map[int]int)
+	ans = bfs(coins, amount)
+	return
+}
+
+var memo map[int]int
+
+func bfs(coins []int, amount int) (ans int) {
+	if val, ok := memo[amount]; ok {
+		ans = val
+		return
+	}
+
+	if amount < 0 {
+		return -1
+	}
+
+	if amount == 0 {
+		return 0
+	}
+
+	ans = math.MaxInt
+	for _, coin := range coins {
+		cnt := bfs(coins, amount-coin)
+		if cnt >= 0 {
+			ans = min(ans, cnt+1)
+		}
+	}
+
+	if ans == math.MaxInt {
+		ans = -1
+	}
+	memo[amount] = ans
+	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
--- agthm/lc0322/lc0322_test.go
+++ agthm/lc0322/lc0322_test.go
@@ -1,1 +1,21 @@
 package lc0322
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_LC0322_01(t *testing.T) {
+	coins := []int{1, 2, 5}
+	amount := 11
+	ans := coinChange(coins, amount)
+	assert.Equal(t, 3, ans)
+}
+
+func Test_LC0322_02(t *testing.T) {
+	coins := []int{186, 419, 83, 408}
+	amount := 6249
+	ans := coinChange(coins, amount)
+	assert.Equal(t, 20, ans)
+}
