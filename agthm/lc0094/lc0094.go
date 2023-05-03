4e5277829df164b31f7e02b5025526944c25a4a2
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-04 01:36:14 +0800

    timestamp: 2023-05-04 01:36:14

---
+++ agthm/lc0094/lc0094.go
@@ -0,0 +1,22 @@
+package lc0094
+
+func inorderTraversal(root *TreeNode) (ans []int) {
+	if root == nil {
+		return
+	}
+
+	curr := root
+	stack := []*TreeNode{}
+	for curr != nil || len(stack) > 0 {
+		for curr != nil {
+			stack = append(stack, curr)
+			curr = curr.Left
+		}
+		curr = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		ans = append(ans, curr.Val)
+		curr = curr.Right
+	}
+
+	return
+}
---
+++ agthm/lc0094/lc0094_test.go
@@ -0,0 +1,21 @@
+package lc0094
+
+import (
+	"testing"
+)
+
+func Test_LC0094_01(t *testing.T) {
+	root := &TreeNode{
+		Val: 3,
+		Left: &TreeNode{
+			Val: 9,
+		},
+		Right: &TreeNode{
+			Val:   20,
+			Left:  &TreeNode{Val: 15},
+			Right: &TreeNode{Val: 7},
+		},
+	}
+	ans := inorderTraversal(root)
+	t.Log(ans)
+}
