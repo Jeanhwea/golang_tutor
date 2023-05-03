3f9442996be0f29d805bd6411887f5490e658d04
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-04 01:51:23 +0800

    timestamp: 2023-05-04 01:51:23

---
+++ agthm/lc0145/lc0145.go
@@ -0,0 +1,26 @@
+package lc0145
+
+func postorderTraversal(root *TreeNode) (ans []int) {
+	if root == nil {
+		return
+	}
+
+	var prev *TreeNode
+	curr := root
+	stack := []*TreeNode{}
+	for curr != nil || len(stack) > 0 {
+		for curr != nil {
+			stack = append(stack, curr)
+			curr = curr.Left
+		}
+		top := stack[len(stack)-1]
+		if top.Right == nil || top.Right == prev {
+			ans = append(ans, top.Val)
+			prev = top
+			stack = stack[:len(stack)-1]
+		} else {
+			curr = top.Right
+		}
+	}
+	return
+}
---
+++ agthm/lc0145/lc0145_test.go
@@ -0,0 +1,11 @@
+package lc0145
+
+import (
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func Test_LC0145_01(t *testing.T) {
+    assert.Equal(t, true, true)
+}
