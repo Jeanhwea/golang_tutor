dc2401e92e4c8bff0b25c6c98f70d3c442434eed
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-04 20:25:11 +0800

    timestamp: 2023-05-04 20:25:11

---
+++ topic/tutor05_pointer/tutor05_pointer_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+	"unsafe"
+)
+
+func TestTutor05Pointer01(t *testing.T) {
+	var nums1 int32 = -1
+	// 任何类型的指针都可以转化成 unsafe.Pointer
+	ptr := unsafe.Pointer(&nums1)
+	// unsafe.Pointer 可以转化成任何类型的指针
+	pUint := (*uint32)(ptr)
+	pInt := (*int32)(ptr)
+	t.Log(*pUint)
+	t.Log(*pInt)
+
+	t.Logf("%x\n", *pUint)
+}
