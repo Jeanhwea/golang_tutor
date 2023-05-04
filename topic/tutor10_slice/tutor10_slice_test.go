add97df0b200bd04b0020275e095dac94d849ac4
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-04 12:44:58 +0800

    update

--- topic/tutor10_slice/tutor10_slice_test.go
+++ topic/tutor10_slice/tutor10_slice_test.go
@@ -18,10 +18,13 @@ func f2(a []int) {
 }
 
 func TestTutor10Slice01(t *testing.T) {
-	a := []int{1}
+	// a := []int{1}
+	a := make([]int, 0, 4)
 	t.Logf("a=%v, len=%v, cap=%v\n", a, len(a), cap(a))
-	f1(a)
+	f2(a)
 	t.Logf("a=%v, len=%v, cap=%v\n", a, len(a), cap(a))
 	a = append(a, 2)
 	t.Logf("a=%v, len=%v, cap=%v\n", a, len(a), cap(a))
+
+	t.Logf("a=%p, len=%v, cap=%v\n", &a, len(a), cap(a))
 }
