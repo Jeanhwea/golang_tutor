639c9e7d4a2548fdd3c74b6f844d863711ea25ac
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-07 01:44:34 +0800

    timestamp: 2023-05-07 01:44:34

---
+++ ncode/nc0003/nc0003.go
@@ -0,0 +1,35 @@
+package nc0003
+
+// K 个链表一组翻转
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	dummy := &ListNode{Next: head}
+
+	tail := dummy
+	for curr := head; curr != nil; {
+		var subHead, subTail *ListNode
+		i := 0
+		for ; i < k && curr != nil; i++ {
+			if i == 0 {
+				subHead, subTail, curr.Next, curr = curr, curr, nil, curr.Next
+			} else {
+				curr.Next, subHead, curr = subHead, curr, curr.Next
+			}
+		}
+
+		if i == k {
+			tail.Next, tail = subHead, subTail
+		} else {
+			var newHead *ListNode
+			for curr := subHead; curr != nil; {
+				if newHead == nil {
+					newHead, curr.Next, curr = curr, nil, curr.Next
+				} else {
+					curr.Next, newHead, curr = newHead, curr, curr.Next
+				}
+			}
+			tail.Next = newHead
+		}
+	}
+
+	return dummy.Next
+}
---
+++ ncode/nc0003/nc0003_test.go
@@ -0,0 +1,11 @@
+package nc0003
+
+import (
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+)
+
+func Test_NC0003_01(t *testing.T) {
+    assert.Equal(t, true, true)
+}
