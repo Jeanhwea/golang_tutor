32e2a02d8b2b52776a049d30caa976bf68765160
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-08 01:23:54 +0800

    timestamp: 2023-05-08 01:23:54

--- newcoder/nc0009/nc0009.go
+++ newcoder/nc0009/nc0009.go
@@ -4,8 +4,9 @@ import (
 	. "github.com/jeanhwea/golang_tutor/common/model"
 )
 
-// 删除链表的倒数第n个节点
+// 删除链表的倒数第 n 个节点
 func removeNthFromEnd(head *ListNode, n int) (ans *ListNode) {
+	// 先用快慢指针找到倒数节点
 	head = &ListNode{Next: head}
 	fast, slow := head, head
 	for i := 0; i <= n; i++ {
@@ -19,6 +20,7 @@ func removeNthFromEnd(head *ListNode, n int) (ans *ListNode) {
 		fast, slow = fast.Next, slow.Next
 	}
 
+	// 然后直接删除倒数 n 个节点
 	if slow != nil && slow.Next != nil {
 		slow.Next = slow.Next.Next
 	}
