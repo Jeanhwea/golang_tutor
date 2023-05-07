78f27fe2190c2e9cb83d4e869da76b9319f7a93a
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-07 23:31:23 +0800

    timestamp: 2023-05-07 23:31:23

--- newcoder/nc0005/nc0005.go
+++ newcoder/nc0005/nc0005.go
@@ -5,5 +5,40 @@ import (
 )
 
 func mergeKLists(lists []*ListNode) (ans *ListNode) {
+	return doMerge(lists, 0, len(lists)-1)
+}
+
+func doMerge(lists []*ListNode, left, right int) (out *ListNode) {
+	if left > right {
+		return
+	} else if left == right {
+		return lists[left]
+	}
+
+	// left < right
+	mid := left + (right-left)/2
+	out = merge(doMerge(lists, left, mid), doMerge(lists, mid+1, right))
+	return
+}
+
+// 合并两链表
+func merge(p, q *ListNode) (head *ListNode) {
+	dummy := &ListNode{}
+	tail := dummy
+	for p != nil && q != nil {
+		if p.Val < q.Val {
+			tail.Next, p.Next, tail, p = p, nil, p, p.Next
+		} else {
+			tail.Next, q.Next, tail, q = q, nil, q, q.Next
+		}
+	}
+
+	if p == nil {
+		tail.Next = q
+	} else {
+		tail.Next = p
+	}
+
+	head = dummy.Next
 	return
 }
