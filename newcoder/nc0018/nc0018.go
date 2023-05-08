31da28225cb7f9ec8cd391f426d13e9e2b9db93d
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-09 01:15:09 +0800

    timestamp: 2023-05-09 01:15:09

---
+++ newcoder/nc0018/nc0018.go
@@ -0,0 +1,21 @@
+package nc0018
+
+import (
+	. "github.com/jeanhwea/golang_tutor/common/model"
+)
+
+// 二维数组中的查找
+func Find(target int, array [][]int) (ans bool) {
+	n, m := len(array), len(array[0])
+	for i, j := n-1, 0; i >= 0 && i < n && j >= 0 && j < m; {
+		if array[i][j] == target {
+			ans = true
+			return
+		} else if array[i][j] < target {
+			j++
+		} else {
+			i--
+		}
+	}
+	return
+}
