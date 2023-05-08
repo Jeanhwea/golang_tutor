9403081bb29674d7c6847f5ca476c07c2635ed54
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-09 01:02:14 +0800

    timestamp: 2023-05-09 01:02:14

---
+++ newcoder/nc0017/nc0017.go
@@ -0,0 +1,23 @@
+package nc0017
+
+import (
+	. "github.com/jeanhwea/golang_tutor/common/model"
+)
+
+// 二分查找-I
+func search(nums []int, target int) (ans int) {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			ans = mid
+			return
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	ans = -1
+	return
+}
