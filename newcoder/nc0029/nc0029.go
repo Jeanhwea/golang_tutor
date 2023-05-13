79b6a49fdcdd68df444ff4f1d36d55e456af1a11
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-13 13:39:49 +0800

    add

--- newcoder/nc0029/nc0029.go
+++ newcoder/nc0029/nc0029.go
@@ -5,5 +5,32 @@ import (
 )
 
 func hasPathSum(root *TreeNode, sum int) bool {
-	return false
+	bfs(root, 0, sum)
+	return found
+}
+
+var (
+	found = false
+)
+
+func bfs(curr *TreeNode, parentSum, sum int) {
+	if found || curr == nil {
+		return
+	}
+
+	currSum := parentSum + curr.Val
+	if curr.Left == nil && curr.Right == nil {
+		if currSum == sum {
+			found = true
+		}
+		return
+	}
+
+	if curr.Left != nil {
+		bfs(curr.Left, currSum, sum)
+	}
+
+	if curr.Right != nil {
+		bfs(curr.Right, currSum, sum)
+	}
 }
