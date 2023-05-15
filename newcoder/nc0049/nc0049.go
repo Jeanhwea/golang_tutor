2a4243f82f9d07f79327ae040fd98819118f9b3f
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-15 23:44:49 +0800

    timestamp: 2023-05-15 23:44:49

--- newcoder/nc0049/nc0049.go
+++ newcoder/nc0049/nc0049.go
@@ -1,19 +1,60 @@
 package nc0049
 
 func solve(s string) (ans int) {
-	var op []byte
-	var nums []int
-	var num, sign int
-	for i, n := 0, len(s); i < n; i++ {
+	ans, _ = eval(s, 0)
+	return
+}
+
+func eval(s string, index int) (val, i int) {
+	var opts []byte // 操作符栈
+	var nums []int  // 操作数
+
+	pushNumber := func(num int) {
+		if len(opts) > 0 && opts[len(opts)-1] == '*' {
+			lhs := nums[len(nums)-1]
+			nums = nums[:len(nums)-1]
+			opts = opts[:len(opts)-1]
+			num = lhs * num
+		}
+		nums = append(nums, num)
+	}
+
+	var num int
+	i = index
+outter:
+	for n := len(s); i < n; i++ {
 		switch ch := s[i]; ch {
-		case '(', '+':
-			op = append(op, ch)
-		default:
-			for num := 0; i < n && s[i] >= '0' && s[i] <= '9'; i++ {
-				num = 10*num + int(s[i]-'0')
+		case '(': // 如果遇到左括号需要递归处理
+			num, i = eval(s, i+1)
+			if s[i] != ')' {
+				panic("right parenthesis miss match!")
 			}
-			nums = append(nums, sign*num)
+			pushNumber(num)
+		case ')': // 右括号跳出解析
+			break outter
+		case '+', '-', '*':
+			opts = append(opts, ch)
+		case ' ':
+			continue
+		default: // 解析数字
+			num = 0
+			for i < n && s[i] >= '0' && s[i] <= '9' {
+				num = 10*num + int(s[i]-'0')
+				i++
+			}
+			pushNumber(num)
+			i--
 		}
 	}
+
+	val = nums[0]
+	for k := 0; k < len(opts); k++ {
+		if opts[k] == '+' {
+			val += nums[k+1]
+		} else if opts[k] == '-' {
+			val += -nums[k+1]
+		}
+	}
+
 	return
 }
--- newcoder/nc0049/nc0049_test.go
+++ newcoder/nc0049/nc0049_test.go
@@ -9,3 +9,23 @@ import (
 func Test_NC0049_01(t *testing.T) {
 	assert.Equal(t, 3, solve("1+2"))
 }
+
+func Test_NC0049_02(t *testing.T) {
+	assert.Equal(t, 13, solve("11+2"))
+}
+
+func Test_NC0049_03(t *testing.T) {
+	assert.Equal(t, 9, solve("11-2"))
+}
+
+func Test_NC0049_04(t *testing.T) {
+	assert.Equal(t, 7, solve("11-2*2"))
+}
+
+func Test_NC0049_05(t *testing.T) {
+	assert.Equal(t, 26, solve("3+2*3*4-1"))
+}
+
+func Test_NC0049_06(t *testing.T) {
+	assert.Equal(t, -10, solve("(2*(3-4))*5"))
+}
