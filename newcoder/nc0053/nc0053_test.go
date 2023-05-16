b8cf35f6363b1e3bf50ca10048f084854656c993
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-16 13:47:32 +0800

    timestamp: 2023-05-16 13:47:32

---
+++ newcoder/nc0053/nc0053_test.go
@@ -0,0 +1,12 @@
+package nc0053
+
+import (
+    "testing"
+
+    . "github.com/jeanhwea/golang_tutor/common/model"
+    "github.com/stretchr/testify/assert"
+)
+
+func Test_NC0053_01(t *testing.T) {
+    assert.Equal(t, true, true)
+}
