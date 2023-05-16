4dce711878512faa409c98a5f4791bf0922b3c68
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-16 22:25:30 +0800

    timestamp: 2023-05-16 22:25:30

--- newcoder/nc0055/nc0055.go
+++ newcoder/nc0055/nc0055.go
@@ -2,19 +2,25 @@ package nc0055
 
 // 没有重复项数字的全排列
 func permute(nums []int) (ans [][]int) {
-	if len(nums) == 1 {
-		ans = append(ans, nums)
+	res = [][]int{}
+	dfs(nums, 0)
+	ans = res
+	return
+}
+
+var (
+	res [][]int
+)
+
+func dfs(nums []int, index int) {
+	if index == len(nums) {
+		res = append(res, append([]int{}, nums...))
 		return
 	}
 
-	n := len(nums)
-	for i := 0; i < n; i++ {
-		nums[0], nums[i] = nums[i], nums[0]
-		num := nums[0]
-		for _, perm := range permute(nums[1:]) {
-			ans = append(ans, append([]int{num}, perm...))
-		}
-		nums[0], nums[i] = nums[i], nums[0]
+	for i := index; i < len(nums); i++ {
+		nums[index], nums[i] = nums[i], nums[index]
+		dfs(nums, index+1)
+		nums[index], nums[i] = nums[i], nums[index]
 	}
-	return
 }
