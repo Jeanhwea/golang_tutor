b9a4b941e07baeb18e4035f7dff7bfb818fd0a85
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-17 17:51:43 +0800

    timestamp: 2023-05-17 17:51:43

--- newcoder/nc0056/nc0056_test.go
+++ newcoder/nc0056/nc0056_test.go
@@ -8,6 +8,16 @@ import (
 
 func Test_NC0056_01(t *testing.T) {
 	ans := permuteUnique([]int{3, 2, 0, 1, 2})
-	t.Log(ans)
+	for i, v := range ans {
+		t.Logf("%3d %v", i+1, v)
+	}
+	assert.Equal(t, true, true)
+}
+
+func Test_NC0056_02(t *testing.T) {
+	ans := permuteUnique([]int{0, 1, 1})
+	for i, v := range ans {
+		t.Logf("%3d %v", i+1, v)
+	}
 	assert.Equal(t, true, true)
 }
