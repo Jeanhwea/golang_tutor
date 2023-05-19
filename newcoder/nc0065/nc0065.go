6de85c4c85c972008e37201f0ea17219b64be03c
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-20 02:24:50 +0800

    timestamp: 2023-05-20 02:24:50

--- newcoder/nc0065/nc0065.go
+++ newcoder/nc0065/nc0065.go
@@ -1,44 +1,37 @@
 package nc0065
 
-const (
-	MAX_LEN = 2001
-)
-
 // 最长公共子序列 (二)
 func LCS(s1 string, s2 string) (ans string) {
 	n, m := len(s1), len(s2)
-	dp := [MAX_LEN][MAX_LEN]int{}
-	choose := [MAX_LEN]bool{}
-	for k := 0; k < max(n, m)*2+1; k++ {
-		for i, j := 0, k-1; i >= 0 && j >= 0; i, j = i+1, j-1 {
-			if i > n || j > m {
-				continue
+	dp := arr2d(n+1, m+1)
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m; j++ {
+			if s1[i-1] == s2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 			}
-			maxlen := 0
-			if i-1 >= 0 && dp[i-1][j] > maxlen {
-				maxlen = dp[i-1][j]
-			}
-			if j-1 >= 0 && dp[i][j-1] > maxlen {
-				maxlen = dp[i][j-1]
-			}
-			if i-1 >= 0 && j-1 >= 0 {
-				b := dp[i-1][j-1]
-				if s1[i-1] == s2[j-1] {
-					b++
-				}
-				if b > maxlen {
-					maxlen = b
-					choose[i-1] = true
-				}
-			}
-			dp[i][j] = maxlen
 		}
 	}
-	for i := 0; i < n; i++ {
-		if choose[i] {
-			ans = ans + string(s1[i])
+
+	var str []byte
+	for i, j := n, m; dp[i][j] > 0; {
+		if s1[i-1] == s2[j-1] {
+			str = append(str, s1[i-1])
+			i--
+			j--
+		} else if dp[i-1][j] > dp[i][j-1] {
+			i--
+		} else {
+			j--
 		}
 	}
+
+	for i, j := 0, len(str)-1; i < j; i, j = i+1, j-1 {
+		str[i], str[j] = str[j], str[i]
+	}
+
+	ans = string(str)
 	return
 }
 
@@ -47,4 +40,11 @@ func max(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func arr2d(n, m int) (ans [][]int) {
+	for i := 0; i < n; i++ {
+		ans = append(ans, make([]int, m))
+	}
+	return
 }
--- newcoder/nc0065/nc0065_test.go
+++ newcoder/nc0065/nc0065_test.go
@@ -12,3 +12,15 @@ func Test_NC0065_01(t *testing.T) {
 	t.Log(ans)
 	assert.Equal(t, true, true)
 }
+
+func Test_NC0065_02(t *testing.T) {
+	s1, s2 := "1A2C3D4B56", "B1D23A456A"
+	ans := LCS(s1, s2)
+	t.Log(ans)
+	assert.Equal(t, true, true)
+}
+
+func TestNc006501(t *testing.T) {
+	ans := arr2d(2, 3)
+	t.Log(ans)
+}
