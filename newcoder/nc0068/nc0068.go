d4d922f543c476623c4490b5ff7077ff1a9a80d8
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-21 00:47:47 +0800

    add

--- newcoder/nc0068/nc0068.go
+++ newcoder/nc0068/nc0068.go
@@ -1,5 +1,28 @@
 package nc0068
 
-import (
-    . "github.com/jeanhwea/golang_tutor/common/model"
-)
+func minPathSum(matrix [][]int) (ans int) {
+	n, m := len(matrix), len(matrix[0])
+	dp := [500][500]int{}
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			if i-1 >= 0 && j-1 >= 0 {
+				dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + matrix[i][j]
+			} else if i-1 >= 0 {
+				dp[i][j] = dp[i-1][j] + matrix[i][j]
+			} else if j-1 >= 0 {
+				dp[i][j] = dp[i][j-1] + matrix[i][j]
+			} else {
+				dp[i][j] = matrix[i][j]
+			}
+		}
+	}
+	ans = dp[n-1][m-1]
+	return
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
