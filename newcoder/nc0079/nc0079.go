d4f5cc55cac1549ae43898b69b8fd39bb3e6f7e3
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-23 23:16:50 +0800

    timestamp: 2023-05-23 23:16:50

--- newcoder/nc0079/nc0079.go
+++ newcoder/nc0079/nc0079.go
@@ -1,5 +1,35 @@
 package nc0079
 
 func rob(nums []int) (ans int) {
+	n := len(nums)
+	if n == 1 {
+		ans = nums[0]
+		return
+	}
+
+	dp := make([]int, len(nums))
+	if nums[0] > nums[1] { // 打劫第 0 家
+		dp[0], dp[1] = nums[0], nums[0]
+		for i := 2; i < n-1; i++ {
+			dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+		}
+		ans = dp[n-2]
+	} else { // 不打劫第 0 家
+		dp[0], dp[1] = 0, nums[1]
+		for i := 2; i < n; i++ {
+			dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+		}
+		if dp[n-1] > ans {
+			ans = dp[n-1]
+		}
+	}
+
 	return
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
--- newcoder/nc0079/nc0079_test.go
+++ newcoder/nc0079/nc0079_test.go
@@ -7,5 +7,16 @@ import (
 )
 
 func Test_NC0079_01(t *testing.T) {
-	assert.Equal(t, true, true)
+	ans := rob([]int{1, 2, 3, 4})
+	assert.Equal(t, 6, ans)
+}
+
+func Test_NC0079_02(t *testing.T) {
+	ans := rob([]int{1, 3, 6})
+	assert.Equal(t, 6, ans)
+}
+
+func Test_NC0079_03(t *testing.T) {
+	ans := rob([]int{69, 27, 25, 44, 1, 16, 76, 98, 22, 52})
+	assert.Equal(t, 237, ans)
 }
