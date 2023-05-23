f3441f71bdc56cd624b41ae70237bbce82c8ef46
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-23 23:46:44 +0800

    timestamp: 2023-05-23 23:46:44

--- newcoder/nc0080/nc0080.go
+++ newcoder/nc0080/nc0080.go
@@ -1,12 +1,7 @@
 package nc0080
 
-import (
-	"math"
-)
-
 // 买卖股票的最好时机(一)
 func maxProfit(prices []int) (ans int) {
-	ans = math.MinInt
 	sum := 0
 	for i := 0; i < len(prices)-1; i++ {
 		sum += prices[i+1] - prices[i]
@@ -16,9 +11,6 @@ func maxProfit(prices []int) (ans int) {
 		if sum > ans {
 			ans = sum
 		}
-	}
-	if ans == math.MinInt {
-		ans = 0
 	}
 	return
 }
