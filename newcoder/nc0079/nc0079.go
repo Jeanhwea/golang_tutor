d6451c3e929f7d17a377faa3949299574e5a4a0e
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-23 23:21:12 +0800

    feat: rob2

--- newcoder/nc0079/nc0079.go
+++ newcoder/nc0079/nc0079.go
@@ -1,5 +1,6 @@
 package nc0079
 
+// 打家劫舍(二)
 func rob(nums []int) (ans int) {
 	n := len(nums)
 	if n == 1 {
@@ -8,21 +9,20 @@ func rob(nums []int) (ans int) {
 	}
 
 	dp := make([]int, len(nums))
-	if nums[0] > nums[1] { // 打劫第 0 家
-		dp[0], dp[1] = nums[0], nums[0]
-		for i := 2; i < n-1; i++ {
-			dp[i] = max(nums[i]+dp[i-2], dp[i-1])
-		}
-		ans = dp[n-2]
-	} else { // 不打劫第 0 家
-		dp[0], dp[1] = 0, nums[1]
-		for i := 2; i < n; i++ {
-			dp[i] = max(nums[i]+dp[i-2], dp[i-1])
-		}
-		if dp[n-1] > ans {
-			ans = dp[n-1]
-		}
+
+	// Case 1: 打劫第 0 家
+	dp[0], dp[1] = nums[0], nums[0]
+	for i := 2; i < n-1; i++ {
+		dp[i] = max(nums[i]+dp[i-2], dp[i-1])
 	}
+	ans = max(ans, dp[n-2])
+
+	// Case 2: 不打劫第 0 家
+	dp[0], dp[1] = 0, nums[1]
+	for i := 2; i < n; i++ {
+		dp[i] = max(nums[i]+dp[i-2], dp[i-1])
+	}
+	ans = max(ans, dp[n-1])
 
 	return
 }
