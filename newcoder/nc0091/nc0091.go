9cc255e8c2d1a9b277f6fc8b704837237648e451
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-25 17:48:11 +0800

    add

---
+++ newcoder/nc0091/nc0091.go
@@ -0,0 +1,9 @@
+package nc0091
+
+func solve(str string) string {
+	res := []byte(str)
+	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
+		res[i], res[j] = res[j], res[i]
+	}
+	return string(res)
+}
---
+++ newcoder/nc0091/nc0091_test.go
@@ -0,0 +1,11 @@
+package nc0091
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NC0091_01(t *testing.T) {
+	assert.Equal(t, true, true)
+}
