b9fbf7b847f22ff47e97f26312bff44a30a6fe43
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-26 17:31:55 +0800

    feat: 双指针维护最长无重复数组子数组

--- newcoder/nc0092/nc0092.go
+++ newcoder/nc0092/nc0092.go
@@ -7,26 +7,29 @@ func maxLength(nums []int) (ans int) {
 
 	i, j := 0, 0 // i, j 表示窗口的左右指针，窗口的范围为 [i,j)
 	for {
-		if j < n && has[nums[j]] { // 如果 nums[j] 已经存在
-			if ans < j-i {
-				ans = j - i
-			}
-
-			// 移动左指针来维护 has 数据结构
-			for ; nums[i] != nums[j]; i++ {
-				has[nums[i]] = false
-			}
-			i++
+		// 窗口扩张: 右移动右指针
+		for j < n && !has[nums[j]] {
+			has[nums[j]] = true
+			j++
 		}
 
+		// 维护最长的结果值
+		if ans < j-i {
+			ans = j - i
+		}
+
+		// 数组遍历完成后跳出循环
 		if j >= n {
-			if ans < j-i {
-				ans = j - i
-			}
 			break
 		}
 
-		has[nums[j]] = true
+		// 窗口收缩: 右移动左指针
+		for i < j && nums[i] != nums[j] {
+			has[nums[i]] = false
+			i++
+		}
+
+		i++
 		j++
 	}
 
