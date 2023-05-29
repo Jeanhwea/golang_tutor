a9fe3f5fa8a765650b4431beb3764c5343aaf4df
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-29 17:50:02 +0800

    timestamp: 2023-05-29 17:50:02

--- newcoder/nc0097/nc0097.go
+++ newcoder/nc0097/nc0097.go
@@ -1,6 +1,28 @@
 package nc0097
 
 func solve(n, m int, a []int) (ans []int) {
+	m = m % n
+
+	// 全部翻转
+	for i, j := 0, n-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+
+	// 翻转 [0, m-1]
+	for i, j := 0, m-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+
+	// 翻转 [m, n-1]
+	for i, j := m, n-1; i < j; i, j = i+1, j-1 {
+		a[i], a[j] = a[j], a[i]
+	}
+
+	ans = a
+	return
+}
+
+func solve2(n, m int, a []int) (ans []int) {
 	ans = make([]int, n)
 	for i := 0; i < n; i++ {
 		ans[(i+m)%n] = a[i]
