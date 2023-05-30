5ab3c3e1f8f9646bd3082be7f4513f2cb3766351
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-31 00:07:10 +0800

    timestamp: 2023-05-31 00:07:10

--- newcoder/nc0101/nc0101.go
+++ newcoder/nc0101/nc0101.go
@@ -2,6 +2,16 @@ package nc0101
 
 // 设计LFU缓存结构
 func LFU(operators [][]int, k int) (ans []int) {
+	n := len(operators)
+	lfu := NewLFU(k)
+	for i := 0; i < n; i++ {
+		switch opt := operators[i][0]; opt {
+		case 1:
+			lfu.Set(operators[i][1], operators[i][2])
+		case 2:
+			ans = append(ans, lfu.Get(operators[i][1]))
+		}
+	}
 	return
 }
 
@@ -16,4 +26,19 @@ type Entry struct {
 	Val  int    // 值
 	Freq int    // 频率
 	Next *Entry // 节点下一个指针
+}
+
+type LFUCache struct {
+}
+
+func NewLFU(capacity int) LFUCache {
+	return LFUCache{}
+}
+
+func (l *LFUCache) Get(key int) (value int) {
+	return
+}
+
+func (l *LFUCache) Set(key, value int) {
+
 }
