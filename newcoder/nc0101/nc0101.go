ba75c0e3f81089dfeec681afa02bd8db84f80f61
Jinghui Hu <hujinghui@buaa.edu.cn>
2023-05-31 00:01:56 +0800

    timestamp: 2023-05-31 00:01:56

--- newcoder/nc0101/nc0101.go
+++ newcoder/nc0101/nc0101.go
@@ -1,1 +1,19 @@
 package nc0101
+
+// 设计LFU缓存结构
+func LFU(operators [][]int, k int) (ans []int) {
+	return
+}
+
+// 思路: 采用双 hash 表来记录数据
+var (
+	freqHash map[int]*Entry // key: 频率, val: 链表头
+	nodeHash map[int]*Entry // key: 键,   val: 元素节点
+)
+
+type Entry struct {
+	Key  int    // 键
+	Val  int    // 值
+	Freq int    // 频率
+	Next *Entry // 节点下一个指针
+}
